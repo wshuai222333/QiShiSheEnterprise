76f5525a40af2dd31529cfc60af5342acf1b47ef
wshuai <wangshuai@reapal.com>
2019-01-08 16:46:02 +0800

    123

--- src/components/managepage/downorder.vue
+++ src/components/managepage/downorder.vue
@@ -2,60 +2,106 @@
     <div>
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-lx-calendar"></i> 订单管理</el-breadcrumb-item>
-                <el-breadcrumb-item>订单下单</el-breadcrumb-item>
+                <el-breadcrumb-item><i class="el-icon-lx-calendar"></i> 表单管理</el-breadcrumb-item>
+                <el-breadcrumb-item>表单</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <div class="container">
             <div class="form-box">
                 <el-form ref="form" :model="form" label-width="80px">
+                <el-form-item >
+                    <el-steps :active="0">
+                    <el-step title="步骤 1" description="这是一段很长很长很长的描述性文字"></el-step>
+                    <el-step title="步骤 2" description="这是一段很长很长很长的描述性文字"></el-step>
+                     <el-step title="步骤 3" description="这段就没那么长了"></el-step>
+                    </el-steps>
+                </el-form-item>
                 <el-form-item label="日期时间">
                         <el-col :span="11">
                             <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
                         </el-col>
                         <el-col class="line" :span="2">-</el-col>
-                        <el-col :span="11">
-                            <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+                         <el-col :span="11">
+                            <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
                         </el-col>
-                    </el-form-item>
-                    <el-form-item label="表单名称">
+                </el-form-item>
+                <el-form-item label="城市选择">
+                        <el-col :span="11">
+                             <el-cascader :options="options" v-model="form.options" style="width: 100%;"></el-cascader>
+                        </el-col>
+                        <el-col class="line" :span="2"><i class="el-icon-arrow-right" ></i></el-col>
+                         <el-col :span="11">
+                             <el-cascader :options="options" v-model="form.options" style="width: 100%;"></el-cascader>
+                        </el-col>
+                </el-form-item> 
+                <el-form-item label="表单名称">
                         <el-input v-model="form.name"></el-input>
-                    </el-form-item>
-                    <el-form-item label="选择器">
-                        <el-select v-model="form.region" placeholder="请选择">
-                            <el-option key="bbk" label="步步高" value="bbk"></el-option>
-                            <el-option key="xtc" label="小天才" value="xtc"></el-option>
-                            <el-option key="imoo" label="imoo" value="imoo"></el-option>
-                        </el-select>
-                    </el-form-item>
-                    
-                    <el-form-item label="城市级联">
-                        <el-cascader :options="options" v-model="form.options"></el-cascader>
-                    </el-form-item>
-                    <el-form-item label="选择开关">
-                        <el-switch v-model="form.delivery"></el-switch>
-                    </el-form-item>
-                    <el-form-item label="多选框">
+                </el-form-item>
+             
+                 <el-form-item>
+                 <p style="border-bottom:solid 1px #ccc;"></p>
+                </el-form-item>
+             
+                <el-form-item label="单选框">
+                        <el-radio-group v-model="form.resource">
+                            <el-radio label="步步高"></el-radio>
+                            <el-radio label="小天才"></el-radio>
+                       </el-radio-group>
+                </el-form-item>
+                <el-form-item label="多选框">
                         <el-checkbox-group v-model="form.type">
                             <el-checkbox label="步步高" name="type"></el-checkbox>
                             <el-checkbox label="小天才" name="type"></el-checkbox>
                             <el-checkbox label="imoo" name="type"></el-checkbox>
                         </el-checkbox-group>
-                    </el-form-item>
-                    <el-form-item label="单选框">
+                
+                </el-form-item>
+                <el-form-item label="表单名称">
+                        <el-input v-model="form.name"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                 <p style="border-bottom:solid 1px #ccc;"></p>
+                </el-form-item>
+
+                <el-form-item label="单选框">
                         <el-radio-group v-model="form.resource">
                             <el-radio label="步步高"></el-radio>
                             <el-radio label="小天才"></el-radio>
                             <el-radio label="imoo"></el-radio>
                         </el-radio-group>
-                    </el-form-item>
-                    <el-form-item label="文本框">
-                        <el-input type="textarea" rows="5" v-model="form.desc"></el-input>
-                    </el-form-item>
-                    <el-form-item>
+                </el-form-item>
+                 <el-form-item label="表单名称">
+                        <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                 <el-form-item>
+                 <p style="border-bottom:solid 1px #ccc;"></p>
+                </el-form-item>
+                <el-form-item label="城市级联" >
+                     <el-row>
+                     <el-col :span="6" >
+                      <el-cascader :options="options" v-model="form.options"></el-cascader>
+                     </el-col>
+                      <el-col :span="6" :offset="1">
+                      <el-cascader :options="options" v-model="form.options"></el-cascader>
+                     </el-col>
+                      <el-col :span="2" :offset="1">
+                      
+                      <el-input v-model="form.name"></el-input> 
+                    
+                     </el-col>
+                     <el-col :span="2">
+                      间             
+                     </el-col>
+                      <el-col :span="6">
+                     <el-button type="primary" icon="el-icon-plus" size="small">添加同行人</el-button>
+                     </el-col>
+                     </el-row>
+              </el-form-item>
+
+                <el-form-item>
                         <el-button type="primary" @click="onSubmit">表单提交</el-button>
-                        <el-button>取消</el-button>
-                    </el-form-item>
+                </el-form-item>
                 </el-form>
             </div>
         </div>
