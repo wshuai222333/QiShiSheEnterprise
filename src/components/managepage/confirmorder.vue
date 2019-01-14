369e0b9af78deb81ddb957ad9c1cbbb832b206b6
wshuai <wangshuai@reapal.com>
2019-01-14 09:37:44 +0800

    11

--- src/components/managepage/confirmorder.vue
+++ src/components/managepage/confirmorder.vue
@@ -20,6 +20,33 @@
                     </el-table-column>
                 </el-table-column>
             </el-table>
+            
+            <el-table :data="tableData3" style="width: 100%">
+                <el-table-column prop="date" label="日期" width="150"></el-table-column>
+                <el-table-column label="配送信息">
+                    <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+                    <el-table-column label="地址">
+                        <el-table-column prop="province" label="省份" width="120"></el-table-column>
+                        <el-table-column prop="city" label="市区" width="120"></el-table-column>
+                        <el-table-column prop="address" label="地址" width="300"></el-table-column>
+                        <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+                    </el-table-column>
+                </el-table-column>
+            </el-table>
+        </div>
+        <div class="container">
+            <el-table :data="tableData3" style="width: 100%">
+                <el-table-column prop="date" label="日期" width="150"></el-table-column>
+                <el-table-column label="配送信息">
+                    <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+                    <el-table-column label="地址">
+                        <el-table-column prop="province" label="省份" width="120"></el-table-column>
+                        <el-table-column prop="city" label="市区" width="120"></el-table-column>
+                        <el-table-column prop="address" label="地址" width="300"></el-table-column>
+                        <el-table-column prop="zip" label="邮编" width="120"></el-table-column>
+                    </el-table-column>
+                </el-table-column>
+            </el-table>
         </div>
     </div>
 </template>
@@ -28,7 +55,6 @@
 export default {
     data() {
         return {
-          
             tableData3: [
                 {
                     date: "2016-05-03",
@@ -43,3 +69,28 @@ export default {
     }
 };
 </script>
+<style scoped>
+    .handle-box {
+        margin-bottom: 20px;
+    }
+
+    .handle-select {
+        width: 120px;
+    }
+
+    .handle-input {
+        width: 300px;
+        display: inline-block;
+    }
+    .del-dialog-cnt{
+        font-size: 16px;
+        text-align: center
+    }
+    .table{
+        width: 100%;
+        font-size: 14px;
+    }
+    .red{
+        color: #ff0000;
+    }
+</style>
